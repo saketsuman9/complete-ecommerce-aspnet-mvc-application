35b537528155574dfe7eff4142800f36adcaabda
Saket Suman <saketsuman9@gmail.com>
2023-07-09 19:26:33 +0530

    new update part 1

---
+++ eTickets/Controllers/CinemasController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eTickets.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace eTickets.Controllers
+{
+    public class CinemasController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public CinemasController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var allproduce = await _context.Cinemas.ToListAsync();
+            return View();
+        }
+    }
+}
---
+++ eTickets/Controllers/ProducerController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eTickets.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace eTickets.Controllers
+{
+    public class ProducerController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ProducerController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var allproduce = await _context.Producers.ToListAsync();
+            return View();
+        }
+    }
+}
